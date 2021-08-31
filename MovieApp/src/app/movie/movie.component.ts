e4927b69713496ce827eefa42b5e3793ba66e30a
Furkan-Zengin <0furkanzengin0@gmail.com>
2021-08-31 22:50:37 +0300

    template creating

--- MovieApp/src/app/movie/movie.component.ts
+++ MovieApp/src/app/movie/movie.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
+  /*styles:['h2{color:blue}']*/
 })
 export class MovieComponent implements OnInit {
 
