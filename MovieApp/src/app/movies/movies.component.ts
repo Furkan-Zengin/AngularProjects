15e523a86370af3c5b0b7e4b8ad701ce428efa5c
Furkan-Zengin <0furkanzengin0@gmail.com>
2021-09-05 16:16:20 +0300

    observable

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -3,25 +3,27 @@ import { Movie } from "../movie";
 import { MovieService } from "../movie.service";
 
 @Component({
-  selector:'movies',
+  selector: 'movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent{
- title =  'Movie List';
- movies :Movie[] = [];
- constructor(private movieService:MovieService){};
- ngOnInit(): void {
-   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-   //Add 'implements OnInit' to the class.
-   this.getMovies();
- }
+export class MoviesComponent {
+  title = 'Movie List';
+  movies: Movie[] = [];
+  constructor(private movieService: MovieService) { };
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getMovies();
+  }
 
- selectedMovie : Movie = new Movie(1,"asd");
- onSelect(movie:Movie):void{
-  this.selectedMovie = movie;
- }
- getMovies():void{
-  this.movies = this.movieService.getMovies();
-}
+  selectedMovie: Movie = new Movie(1, "asd");
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
+  getMovies(): void {
+    this.movieService.getMovies().subscribe(movies => {
+      this.movies = movies
+    });
+  }
 }
